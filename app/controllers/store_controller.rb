af5f5dd3d527bcf2fd09c573a333383dbec73634
Tim Cleary <timothyacleary@gmail.com>
2014-04-27 17:06:41 -0400

    Ch9 Playtime
    - added session counter for store_controller
    - added counter to template if counter >= 5
    - added resets counter if add item to cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,17 @@
 class StoreController < ApplicationController
+  before_action :session_counter, only: [:index]
+
   def index
   	@products = Product.order(:title)
+  end
+
+  private
+
+  def session_counter
+  	if session[:counter].nil?
+  		session[:counter] = 1
+  	else
+  		session[:counter] += 1
+  	end
   end
 end
