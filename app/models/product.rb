38fb86e0a582044a5c8878bf2e1fbd3dc6fbad17
Tim Cleary <timothyacleary@gmail.com>
2014-04-27 16:48:19 -0400

    End of Ch9

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
+	has_many :line_items
+
+	# check that no line items contain the product before allowing delete
+	before_destroy :ensure_not_referenced_by_line_item
+
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01} 
 	validates :title, uniqueness: true
@@ -9,5 +14,16 @@ class Product < ActiveRecord::Base
 
 	def self.latest
 		Product.order(:updated_at).last
+	end
+
+	private
+
+	def ensure_not_referenced_by_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'Line items present')
+			return false
+		end
 	end
 end
