225b39239007573f252e6ad39e7b6d1b3cf2b3cd
Tim Cleary <timothyacleary@gmail.com>
2014-04-27 13:58:18 -0400

    Add date and time to sidebar

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Time::DATE_FORMATS[:access_time] = "Accessed: %B %d at %I:%M %p"
